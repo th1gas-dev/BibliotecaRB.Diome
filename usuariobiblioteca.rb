e0b2771e28582732f9c2adc93c56e3860a2faf3b
th1gas-dev <thigas.ncorreia@gmail.com>
2024-10-29 06:00:25 -0300

    Add files via upload

---
+++ usuariobiblioteca.rb
@@ -0,0 +1,7 @@
+print "Bem vind@ a Biblioteca Central, por favor, digite o seu nome: "
+    nome = gets.chomp
+print "Para facilitar a localização do seu cadastro, digite o seu sobrenome: "
+    sobrenome = gets.chomp
+print "Localizamos o seu cadastro #{nome} #{sobrenome} Iremos te recomendar as melhores  opções de livros para a sua idade. Por favor, nos diga quantos anos você tem: "
+    idade = gets.chomp
+    puts "Bem vind@ #{nome} #{sobrenome}, agora que sabemos que você tem #{idade} te recomendaremos os melhores livros."
